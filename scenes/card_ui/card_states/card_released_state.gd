ae0ce895bbd1d6becd00547c56d29b8efe524f22
Jasper Tan <jaspertan@utexas.edu>
2024-07-24 00:59:05 -0400

    Add files via file upload

---
+++ scenes/card_ui/card_states/card_released_state.gd
@@ -0,0 +1,21 @@
+extends CardState
+
+var played: bool
+
+
+func enter() -> void:
+	card_ui.color.color = Color.DARK_VIOLET
+	card_ui.state.text = "RELEASED"
+
+	played = false
+	
+	if not card_ui.targets.is_empty():
+		played = true
+		print("played card for target(s)", card_ui.targets)
+		
+		
+func on_input(_event: InputEvent) -> void:
+	if played:
+		return
+
+	transition_requested.emit(self, CardState.State.BASE)
---
+++ scenes/card_ui/card_ui.gd
@@ -0,0 +1,48 @@
+class_name CardUI
+extends Control
+
+signal reparent_requested(which_card_ui: CardUI)
+
+@export var card: Card
+
+@onready var color: ColorRect = $Color
+@onready var state: Label = $State
+@onready var drop_point_detector: Area2D = $DropPointDetector
+@onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
+@onready var targets: Array[Node] = []
+
+var parent: Control
+var tween:  Tween
+
+
+func _ready() -> void:
+	card_state_machine.init(self)
+	
+	
+func _input(event: InputEvent) -> void:
+	card_state_machine.on_input(event)
+	
+	
+func animate_to_position(new_position: Vector2, duration: float) -> void:
+	tween = create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "global_position", new_position, duration)
+	
+	
+func _on_gui_input(event: InputEvent) -> void:
+	card_state_machine.on_gui_input(event)
+	
+
+func _on_mouse_entered() -> void:
+	card_state_machine.on_mouse_entered()
+	
+	
+func _on_mouse_exited() -> void:
+	card_state_machine.on_mouse_exited()
+
+
+func _on_drop_point_detector_area_entered(area: Area2D) -> void:
+	if not targets.has(area):
+		targets.append(area)
+
+func _on_drop_point_detector_area_exited(area: Area2D) -> void:
+	targets.erase(area)
